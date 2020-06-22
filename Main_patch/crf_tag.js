9b410c4930d7bb305487f3d94e850cc5af7bcadb
aparment <arthur.parmentier@protonmail.com>
2020-06-23 00:28:16 +0200

    a bit of cleaning

--- Main_patch/crf_tag.js
+++
@@ -1,9 +0,0 @@
-const maxApi = require("max-api");
-const crfsuite = require('crfsuite')
-const tagger = new crfsuite.Tagger()
-
-let is_opened = tagger.open('./path/to/crf.model')
-console.log('File model is opened:', is_opened)
-
-let tags = tagger.tag(input)
-console.log('Tags: ', tags)
